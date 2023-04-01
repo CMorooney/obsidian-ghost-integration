39b1fab795caf96491e72805006f3a1a4bf85c30
calvin <calvin@calvins-MacBook-Pro-2.local>
2023-04-01 08:58:20 -0400

    wip try and parse/dump selected file front matter in console

--- main.ts
+++ main.ts
@@ -1,24 +1,42 @@
-import { Notice, Plugin } from 'obsidian';
+import { Notice, Plugin, FileManager, TAbstractFile, TFile } from 'obsidian';
 import { PluginSettings, DEFAULT_SETTINGS } from 'src/settings/settings';
 
 export default class GhostIntegrationPlugin extends Plugin {
   settings: PluginSettings;
+  fileManager = new FileManager();
 
   async onload() {
     await this.loadSettings();
 
     this.registerEvent(
-      this.app.workspace.on("file-menu", (menu, file) => {
+      this.app.workspace.on("file-menu", (menu, file: TAbstractFile) => {
+        const actualFile = file as TFile;
+        if(!actualFile) {
+          new Notify("unable to read file");
+          return;
+        }
+
         menu.addItem((item) => {
           item
             .setTitle("Publish to Ghost")
             .setIcon("ghost")
-            .onClick(async () => {
-              new Notice(file.path);
-            });
+            .onClick(() => this.tryToPublishToGhost(actualFile));
         });
       })
     );
+  }
+
+  tryToPublishToGhost(file: TFile) {
+    try {
+      this.fileManager.processFrontMatter(file, (data: any) => {
+        console.log(`----- front matter processed:`);
+        console.dir(data);
+      });
+    } catch(e) {
+      new Notify("unexpected problem processing meta data for file");
+      console.log(`***** error 1:`);
+      console.dir(e)
+    }
   }
 
   onunload() { }
